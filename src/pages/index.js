242ca3905fa0c8e6fd1a9ae0c2e9bf4a440510b4
Preston So <pso@post.harvard.edu>
2021-10-04 10:03:46 -0300

    Use GatsbyImage component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,26 @@
 import React from "react"
-import { StaticImage } from "gatsby-plugin-image"
+import { graphql } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
-export default function Home() {
-  return (
-    <StaticImage
-      src="../images/pikes-peak.jpg"
-      alt="Pikes Peak, known as Tava in the Ute language and Heey-otoyoo' in the Arapaho language"
-      placeholder="blurred"
-      layout="fixed"
-    />
-  )
+export default function Home({ data }) {
+  const image = getImage(data.file)
+  return <GatsbyImage
+    image={image}
+    alt="Pikes Peak, known as Tava in the Ute language and Heey-otoyoo' in the Arapaho language"
+  />
 }
+
+export const query = graphql`
+  {
+    file(relativePath: {
+      eq: "pikes-peak.jpg"
+    }) {
+      childImageSharp {
+        gatsbyImageData(
+          width: 300
+          placeholder: BLURRED
+        )
+      }
+    }
+  }
+`
